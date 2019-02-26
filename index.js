3a7c81da8934c03235502854e7568bbd040291f4
ryhoneydew <lucia.yang88@gmail.com>
2019-02-25 22:10:05 -0500

    display photos

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import "babel-polyfill";
-import { getGalleries } from "./utils";
-const APIKEY = "ed6aab79abedf1588ab39d51b9793bb1";
+import { getGalleries, photoUrlGenerator } from "./utils";
+// const APIKEY = "ed6aab79abedf1588ab39d51b9793bb1";
 
-const endPointUrl = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed6aab79abedf1588ab39d51b9793bb1&gallery_id=66911286-72157703406532791&format=json&nojsoncallback=1`;
-//const endPointUrl = `https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=abc&format=json`;
+const peopleShotsUrl = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed6aab79abedf1588ab39d51b9793bb1&gallery_id=66911286-72157705347519995&format=json&nojsoncallback=1`;
 
-getGalleries(endPointUrl);
+const photoCollection = document.getElementById("photo-collection");
+
+getGalleries(peopleShotsUrl).then(data => {
+  data.photos.photo.forEach(item => {
+    var farmId = item.farm;
+    var serverId = item.server;
+    var id = item.id;
+    var secret = item.secret;
+
+    const img = document.createElement("img");
+    const imgUrl = photoUrlGenerator(farmId, serverId, id, secret);
+    img.setAttribute("src", imgUrl);
+    photoCollection.appendChild(img);
+  });
+});
--- utils.js
+++ utils.js
@@ -7,3 +7,7 @@ export const getGalleries = async endpoint => {
     throw new Error("Failed to fetch data ", err.message);
   }
 };
+
+export const photoUrlGenerator = (farmId, serverId, id, secret) =>
+  // https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret +
+  `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`;
