ac885761afc3b5094256ea46c70908958d6d8215
ryhoneydew <lucia.yang88@gmail.com>
2019-02-26 21:30:25 -0500

    add pagination

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "babel-polyfill";
-import { getGalleries, photoUrlGenerator, removeNodes } from "./utils";
+import { getGalleries, removeNodes, Page } from "./utils";
 
 const peopleShotsUrl = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed6aab79abedf1588ab39d51b9793bb1&gallery_id=66911286-72157705347519995&format=json&nojsoncallback=1`;
 const architectureUrl = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed6aab79abedf1588ab39d51b9793bb1&gallery_id=66911286-72157699121913970&format=json&nojsoncallback=1`;
@@ -10,34 +10,39 @@ const photosNode = document.createElement("div");
 const blackWhiteButton = document.getElementById("black-white");
 const architectureButton = document.getElementById("architecture");
 const peopleShotsButton = document.getElementById("people-shots");
+const paginations = document.getElementById("pagination");
 
 const createCollection = async (url, parent) => {
   const data = await getGalleries(url);
-
+  const photos = data.photos.photo;
   const photoNodeList = document.querySelectorAll(".photo");
+  const pgList = document.querySelectorAll(".pg-number");
   let lengthOfPhotoNodes = photoNodeList.length;
-
-  // while (lengthOfPhotoNodes) {
-  //   photosNode.removeChild(photosNode.childNodes[0]);
-  //   lengthOfPhotoNodes--;
-  // }
+  let lengthOfPgList = pgList.length;
 
   removeNodes(lengthOfPhotoNodes, photosNode);
-  data.photos.photo.forEach(item => {
-    var farmId = item.farm;
-    var serverId = item.server;
-    var id = item.id;
-    var secret = item.secret;
+  removeNodes(lengthOfPgList, paginations);
 
-    const elem = document.createElement("div");
-    elem.className = "photo";
-    const img = document.createElement("img");
-    const imgUrl = photoUrlGenerator(farmId, serverId, id, secret);
-    img.setAttribute("src", imgUrl);
-    elem.appendChild(img);
+  const newPage = new Page(photos, 10, photosNode);
 
-    photosNode.appendChild(elem);
-  });
+  newPage.init();
+
+  // data.photos.photo.forEach((item, idx) => {
+  //   const farmId = item.farm;
+  //   const serverId = item.server;
+  //   const id = item.id;
+  //   const secret = item.secret;
+
+  //   const elem = document.createElement("div");
+  //   elem.className = "photo";
+  //   elem.setAttribute("id", `photo${idx}`);
+  //   const img = document.createElement("img");
+  //   const imgUrl = photoUrlGenerator(farmId, serverId, id, secret);
+  //   img.setAttribute("src", imgUrl);
+  //   elem.appendChild(img);
+
+  //   photosNode.appendChild(elem);
+  // });
   parent.appendChild(photosNode);
 };
 
@@ -50,4 +55,3 @@ architectureButton.addEventListener("click", () =>
 peopleShotsButton.addEventListener("click", () =>
   createCollection(peopleShotsUrl, photoCollection)
 );
-// createCollection(architectureUrl, photoCollection);
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import "babel-polyfill";
+import { removeListener } from "cluster";
 export const getGalleries = async endpoint => {
   try {
     const response = await fetch(endpoint);
@@ -13,8 +15,104 @@ export const photoUrlGenerator = (farmId, serverId, id, secret) =>
   `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`;
 
 export const removeNodes = (length, parentNode) => {
+  console.log(parentNode);
+  console.log(length);
   while (length) {
     parentNode.removeChild(parentNode.childNodes[0]);
     length--;
   }
 };
+
+export class Page {
+  constructor(photosList, numberPerPage, photosNode) {
+    this.photosNode = photosNode;
+    this.photosList = photosList;
+    this.currPhotosList = [];
+    this.currentPage = 1;
+    this.numberPerPage = numberPerPage;
+    this.totalPageNum = 0;
+
+    this.getNumberOfPages.bind(this);
+    this.createPaginationButtons.bind(this);
+    this.loadList.bind(this);
+    this.createNodes.bind(this);
+    this.nextPage.bind(this);
+    this.previousPage.bind(this);
+    this.pickPage.bind(this);
+  }
+
+  init() {
+    this.getNumberOfPages();
+    this.loadList();
+    this.createPaginationButtons();
+  }
+  getNumberOfPages() {
+    this.totalPageNum = Math.ceil(this.photosList.length / this.numberPerPage);
+  }
+
+  createPaginationButtons() {
+    const paginationSection = document.getElementById("pagination");
+
+    const previousButton = document.createElement("button");
+    previousButton.innerText = "previous";
+    previousButton.setAttribute("id", "previous");
+    previousButton.className = "pg-number";
+    previousButton.addEventListener("click", () => this.previousPage());
+    paginationSection.appendChild(previousButton);
+
+    for (let i = 1; i <= this.totalPageNum; i++) {
+      const numberButton = document.createElement("button");
+      numberButton.className = "pg-number";
+      numberButton.addEventListener("click", event => this.pickPage(event));
+      numberButton.innerText = i;
+      paginationSection.appendChild(numberButton);
+    }
+    const nextButton = document.createElement("button");
+    nextButton.innerText = "next";
+    nextButton.setAttribute("id", "next");
+    nextButton.className = "pg-number";
+    nextButton.addEventListener("click", () => this.nextPage());
+    paginationSection.appendChild(nextButton);
+  }
+
+  loadList() {
+    removeNodes(this.currPhotosList.length, this.photosNode);
+    const begin = (this.currentPage - 1) * this.numberPerPage;
+    const end = begin + this.numberPerPage;
+    this.currPhotosList = this.photosList.slice(begin, end);
+    this.createNodes();
+  }
+
+  createNodes() {
+    this.currPhotosList.forEach(item => {
+      const farmId = item.farm;
+      const serverId = item.server;
+      const id = item.id;
+      const secret = item.secret;
+
+      const elem = document.createElement("div");
+      elem.className = "photo";
+      const img = document.createElement("img");
+      const imgUrl = photoUrlGenerator(farmId, serverId, id, secret);
+      img.setAttribute("src", imgUrl);
+      elem.appendChild(img);
+
+      this.photosNode.appendChild(elem);
+    });
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.loadList();
+  }
+
+  previousPage() {
+    this.currentPage--;
+    this.loadList();
+  }
+
+  pickPage(event) {
+    this.currentPage = Number(event.target.innerText);
+    this.loadList();
+  }
+}
